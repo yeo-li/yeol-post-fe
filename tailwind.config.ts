d685d3fbf51ebbfe89faeea2080775ac243b90a4
yeo-li <parkseongyeol2110@gmail.com>
2025-08-07 17:17:59 +0900

    fix: @tailwindcss/line-clamp 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -93,6 +93,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate"), require('@tailwindcss/line-clamp')],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
